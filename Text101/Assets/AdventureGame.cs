de794d210a72e4ca6c1770fbf74ce887d7ed18f9
i-z <IvanZdravkovBG@gmail.com>
2019-07-21 20:13:33 +0300

    Programly add text.

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.textComponent.text = "Greetings, space traveler!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
